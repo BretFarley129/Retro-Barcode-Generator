74891312e3da8f4b52755907f3f3579b4ebed3a1
Bret Farley <bretfarley12@mittymonarchl.com>
2017-11-03 18:09:33 -0700

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,21 @@
 import { Component } from '@angular/core';
 
+var arr= [
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'indigo',
+  'violet',
+  'greenyellow',
+  'wheat',
+  'maroon',
+];
+var thing = [];
+for (var i =0; i < arr.length; i++){
+  thing.push(arr[Math.floor(Math.random()*10)])
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +23,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  colors = thing;
+
 }
